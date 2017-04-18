33806d76f62dc3fe89c4763c73f9a6d3c65947d8
Theliahh <garrettmusto@gmail.com>
2017-04-18 00:09:14 -0400

    added timer

--- ParallelSorting/ParallelSort.cs
+++ ParallelSorting/ParallelSort.cs
@@ -4,33 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ParallelSorting
 {
     class ParallelSort
     {
         static int[] unsortedElements = new int[10000];
+        static int numThreads = 1;
+        static ManualResetEvent resetEvent;
+
         static void Main(string[] args)
         {
-            
+
+            Stopwatch sw = new Stopwatch();
+            resetEvent = new ManualResetEvent(false);
             Random rng = new Random();
+
             for(int i = 0; i < unsortedElements.Length; i++)
             {
                 unsortedElements[i] = rng.Next(1, 1000000);
             }
-
+            sw.Start();
             ThreadStart threadRef = new ThreadStart(ThreadTest);
             Thread testThread = new Thread(threadRef);
             testThread.Start();
+
+            resetEvent.WaitOne();
+            sw.Stop();
+
+            Console.WriteLine("Elapsed={0}", sw.Elapsed);
             Console.ReadKey();
         }
 
         public static void ThreadTest()
         {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
             int[] sorted = insertionSort(unsortedElements);
-            for(int i = 0; i< sorted.Length; i++)
+            /*for(int i = 0; i< sorted.Length; i++)
             {
                 Console.Write("{0} ",sorted[i]);
+            }*/
+            if(Interlocked.Decrement(ref numThreads) == 0)
+            {
+                resetEvent.Set();
             }
             
         }
